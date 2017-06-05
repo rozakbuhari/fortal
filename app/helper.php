6118595069fc70c2530d6b4da731368418ce0c6e
Fathurozak Buhari <rozakbuhari@gmail.com>
2017-06-05 09:45:44 +0700

    Clean-up cache folder

--- app/helper.php
+++ app/helper.php
@@ -3,6 +3,6 @@
 use Jenssegers\Blade\Blade;
 
 function view($filename, $data = []) {
-    $blade = new Blade(VIEW_FOLDER, APP. 'cache');
+    $blade = new Blade(VIEW_FOLDER, ROOT . '.cache');
     return $blade->make($filename, $data);
 }
